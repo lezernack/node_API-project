da78bed04c06ffebae445fdc985889c98a50693d
lezernack <luks.ezck@gmail.com>
2024-04-24 12:57:56 -0600

    changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const mongodb = require("./db/connect");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors()).use("/", require("./routes"));
 
