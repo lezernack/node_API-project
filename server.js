203f5038c158727aa84799dd239bb1412019ca6e
lezernack <luks.ezck@gmail.com>
2024-04-23 18:39:09 -0600

    put in code in server.js and connect.js

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const cors = require("cors");
+const mongodb = require("./db/connect");
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
-app.use("/", require("./routes"));
+app.use(cors()).use("/", require("./routes"));
 
-app.listen(PORT, () => {
-  console.log(`Test server running on port ${PORT}`);
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(PORT);
+    console.log(
+      "\x1b[35m%s\x1b[0m",
+      `Connected to DB and listening on ${PORT}`
+    );
+  }
 });
